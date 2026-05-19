678e0af5701bb5b59abb8849317ae16aa50165ef
Esteban Cairol <ecairol@gmail.com>
2026-05-19 09:39:04 -0600

    Bumped version in GIVING_DAY_BLOCKS_VERSION

--- giving-day-blocks.php
+++ giving-day-blocks.php
@@ -26,7 +26,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'GIVING_DAY_BLOCKS_VERSION', '0.4.0' );
+define( 'GIVING_DAY_BLOCKS_VERSION', '1.0.1' );
 define( 'GIVING_DAY_BLOCKS_BASENAME', plugin_basename( __FILE__ ) );
 define( 'GIVING_DAY_BLOCKS_PATH', plugin_dir_path( __FILE__ ) );
 define( 'GIVING_DAY_BLOCKS_URL', plugin_dir_url( __FILE__ ) );
