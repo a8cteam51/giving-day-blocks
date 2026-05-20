460d4379b572143aaf7037616a4e8b428aa614c2
Esteban Cairol <ecairol@gmail.com>
2026-05-20 13:21:44 -0600

    Fixed broken blocks
    - Goal block was empty on the donate page
    - Numbers were too big when placed on sidebar

--- blocks/build/goal-progress/index.asset.php
+++ blocks/build/goal-progress/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '96de453dcdf27c18ce18');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'aec2df1c18eeb2123947');
